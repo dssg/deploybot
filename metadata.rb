1c2b91bb9741728081bce82f6096cf1cc8254e4a
hunterowens <owens.hunter@gmail.com>
2014-06-01 20:51:25 +0800

    templates

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'deploybot'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures deploybot'
+long_description 'Installs/Configures deploybot'
+version          '0.1.0'
+
