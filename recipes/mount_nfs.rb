7112787b11345c4bfdc7de48ad968bc4f223f4ea
zar1 <zachary.rubenstein@gmail.com>
2015-05-14 17:29:49 -0500

    changing mounted volume to data3

--- recipes/mount_nfs.rb
+++ recipes/mount_nfs.rb
@@ -3,28 +3,42 @@
 #
 #
 
+# directory "/mnt/data" do
+#   owner "root"
+#   group "root"
+#   mode 0755
+#   action :create
+# end
+# 
+# mount "/mnt/data" do
+#   device "54.214.253.37:/mnt/data"
+#   fstype "nfs"
+#   options "rw"
+# end
+# 
+# directory "/mnt/data2" do
+#   owner "root"
+#   group "root"
+#   mode 0755
+#   action :create
+# end
+# 
+# mount "mnt/data2" do
+#   device "54.214.253.37:/mnt/data2"
+#   fstype "nfs"
+#   options "rw"
+# end
+
 directory "/mnt/data" do
   owner "root"
   group "root"
   mode 0755
   action :create
 end
-
-mount "/mnt/data" do
-  device "54.214.253.37:/mnt/data"
+ 
+mount "mnt/data" do
+  device "54.214.253.37:/mnt/data3"
   fstype "nfs"
   options "rw"
 end
 
-directory "/mnt/data2" do
-  owner "root"
-  group "root"
-  mode 0755
-  action :create
-end
-
-mount "mnt/data2" do
-  device "54.214.253.37:/mnt/data2"
-  fstype "nfs"
-  options "rw"
-end
