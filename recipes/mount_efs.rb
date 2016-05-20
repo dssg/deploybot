e9835cd5a62f64866e18f51af90b29582022673e
Joseph Walsh <j.thomas.walsh@gmail.com>
2016-05-20 14:21:07 -0500

    added EFS support

---
+++ recipes/mount_efs.rb
@@ -0,0 +1,44 @@
+# Mounts an NFS drive from the other stack
+# Elastic IP is hardcoded
+#
+#
+
+# directory "/mnt/data" do
+#   owner "root"
+#   group "root"
+#   mode 0755
+#   action :create
+# end
+# 
+# mount "/mnt/data" do
+#   device "54.214.253.37:/mnt/data"
+#   fstype "nfs"
+#   options "rw"
+# end
+# 
+# directory "/mnt/data2" do
+#   owner "root"
+#   group "root"
+#   mode 0755
+#   action :create
+# end
+# 
+# mount "mnt/data2" do
+#   device "54.214.253.37:/mnt/data2"
+#   fstype "nfs"
+#   options "rw"
+# end
+
+directory "/mnt/data" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+ 
+mount "mnt/data" do
+  device "10.0.0.153:/"
+  fstype "nfs"
+  options "rw,acl"
+end
+
