d244d72a096f6edffe403501ac0aa6ae9fe3e4e2
Joseph Walsh <j.thomas.walsh@gmail.com>
2016-05-20 19:41:50 -0500

    edited EFS recipe for clarity

--- recipes/mount_efs.rb
+++ recipes/mount_efs.rb
@@ -1,33 +1,7 @@
-# Mounts an NFS drive from the other stack
+# Mounts AWS EFS 
 # Elastic IP is hardcoded
 #
 #
-
-# directory "/mnt/data" do
-#   owner "root"
-#   group "root"
-#   mode 0755
-#   action :create
-# end
-# 
-# mount "/mnt/data" do
-#   device "54.214.253.37:/mnt/data"
-#   fstype "nfs"
-#   options "rw"
-# end
-# 
-# directory "/mnt/data2" do
-#   owner "root"
-#   group "root"
-#   mode 0755
-#   action :create
-# end
-# 
-# mount "mnt/data2" do
-#   device "54.214.253.37:/mnt/data2"
-#   fstype "nfs"
-#   options "rw"
-# end
 
 directory "/mnt/data" do
   owner "root"
