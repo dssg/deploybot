7007871899f3eed1b9093f1ba0d7880ab71f5b21
Joseph Walsh <j.thomas.walsh@gmail.com>
2016-05-23 00:09:15 -0500

    added commands to explicitly choose anaconda version (2 or 3)

--- recipes/run_anaconda_install.rb
+++ recipes/run_anaconda_install.rb
@@ -1,5 +1,7 @@
 execute "install-anaconda" do
     cwd "/mnt/data/anaconda/"
-    command "python anaconda3_install.py"
     command "python anaconda2_install.py"
+    command "python anaconda3_install.py"
+    command "conda create --name anaconda2 python=2"
+    command "conda create --name anaconda3 python=3"
 end
