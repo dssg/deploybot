3366a21bb1a4fc256d603484025e5be0e990bf67
Joseph Walsh <j.thomas.walsh@gmail.com>
2016-05-24 15:20:01 -0500

    added efs mount for dssg training

---
+++ recipes/mount_efs_training.rb
@@ -0,0 +1,18 @@
+# Mounts AWS EFS 
+# Elastic IP is hardcoded
+#
+#
+
+directory "/mnt/data/training" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+ 
+mount "mnt/data/training" do
+  device "10.0.0.153:/"
+  fstype "nfs4"
+  options "rw,acl"
+end
+
