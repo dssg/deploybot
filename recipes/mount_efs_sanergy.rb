7a19ee8dd03ab6da8b0488e1a2a3ef04c571b882
Joseph Walsh <j.thomas.walsh@gmail.com>
2016-05-24 23:39:01 -0500

    need to mount install_files/ directory to each instance

--- recipes/mount_efs_sanergy.rb
+++ recipes/mount_efs_sanergy.rb
@@ -10,6 +10,19 @@ directory "/mnt/data" do
   action :create
 end
 
+directory "/mnt/data/install_files" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+mount "mnt/data/install_files" do
+  device "10.0.0.153:/install_files"
+  fstype "nfs4"
+  options "rw,acl"
+end
+
 directory "/mnt/data/sanergy" do
   owner "root"
   group "root"
