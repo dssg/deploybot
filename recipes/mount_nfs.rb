790ffbeb54f11d5bb6199dde208325fe98863fcb
Joe Walsh <j.thomas.walsh@gmail.com>
2016-10-24 23:08:17 -0500

    Update mount_nfs.rb

--- recipes/mount_nfs.rb
+++ recipes/mount_nfs.rb
@@ -5,13 +5,11 @@ directory "/mnt/data" do
   group "root"
   mode 0755
   action :create
-  not_if {'/bin/hostname -i | grep 10.0.0.244'}
 end
 
 mount "/mnt/data" do
   device "10.0.0.244:/mnt/data"
   fstype "nfs"
   options "rw,acl"
-  not_if {'/bin/hostname -i | grep 10.0.0.244'}
 end
 
