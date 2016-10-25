6d5adc6f1dd862ab1472d96f6b987c96595a9c6a
Joe Walsh <j.thomas.walsh@gmail.com>
2016-10-24 21:47:54 -0500

    Update mount_nfs.rb
    Updated IP to internal address, and made execution conditional so it doesn't run on the file server

--- recipes/mount_nfs.rb
+++ recipes/mount_nfs.rb
@@ -1,44 +1,17 @@
-# Mounts an NFS drive from the other stack
-# Elastic IP is hardcoded
-#
-#
-
-# directory "/mnt/data" do
-#   owner "root"
-#   group "root"
-#   mode 0755
-#   action :create
-# end
-# 
-# mount "/mnt/data" do
-#   device "54.214.253.37:/mnt/data"
-#   fstype "nfs"
-#   options "rw"
-# end
-# 
-# directory "/mnt/data2" do
-#   owner "root"
-#   group "root"
-#   mode 0755
-#   action :create
-# end
-# 
-# mount "mnt/data2" do
-#   device "54.214.253.37:/mnt/data2"
-#   fstype "nfs"
-#   options "rw"
-# end
+# Internal IP is hardcoded
 
 directory "/mnt/data" do
   owner "root"
   group "root"
   mode 0755
   action :create
+  not_if {'/bin/hostname -i | grep 10.0.0.244'}
 end
- 
+
 mount "mnt/data" do
-  device "54.214.253.37:/mnt/data3"
+  device "10.0.0.244:/mnt/data"
   fstype "nfs"
   options "rw,acl"
+  not_if {'/bin/hostname -i | grep 10.0.0.244'}
 end
 
