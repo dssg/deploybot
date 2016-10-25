37446c401d28f08753631b366ca0f6082ea51173
Joe Walsh <j.thomas.walsh@gmail.com>
2016-10-25 11:50:24 -0500

    Update mount_nfs.rb

--- recipes/mount_nfs.rb
+++ recipes/mount_nfs.rb
@@ -14,27 +14,3 @@ mount "/mnt/data" do
   options "rw,acl"
   not_if "/bin/hostname -i | grep 10.0.0.244"
 end
-
-group "public_safety" do
-  gid 7016
-  action :manage
-  members ['jwalsh']
-end
-
-#directory "/mnt/data/public_safety" do
-#  group 'public_safety'
-#  mode 0770
-#  not_if "/bin/hostname -i | grep 10.0.0.244"
-#end
-#
-#directory "/mnt/data/public_safety/charlotte_pd" do
-#  group 'charlotte_pd'
-#  mode 2770
-#  not_if "/bin/hostname -i | grep 10.0.0.244"
-#end
-#
-#directory "/mnt/data/public_safety/nashville_pd" do
-#  group 'nashville_pd'
-#  mode 2770
-#  not_if "/bin/hostname -i | grep 10.0.0.244"
-#end
